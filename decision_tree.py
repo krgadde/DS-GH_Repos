8f6d5f318391a1be0cade26934ff533e65538793
krgadde <karuna.gadde@gmail.com>
2018-12-26 16:34:16 +0530

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -5,16 +5,57 @@ Created on Wed Dec 26 06:23:28 2018
 @author: karun
 """
 from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+
+
 # [height, weight, shoe_size]
 X=[[181, 80, 44],[177, 70, 43],[160, 60, 38],[154, 54, 37], [166, 65, 40],[190, 90, 47],[175, 64, 39],[177, 70, 40],[159, 55, 37],[171, 75, 42],[181, 85, 43]]
 
 Y=['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female','female', 'male', 'male']
 
 
-clf = tree.DecisionTreeClassifier()
+clf_tree = tree.DecisionTreeClassifier()
+clf_svm=SVC()
+clf_perceptron=Perceptron()
+clf_KNN=KNeighborsClassifier()
 
-clf=clf.fit(X,Y)
 
-prediction =clf.predict([[190, 70, 43]])
+clf_tree.fit(X,Y)
+clf_svm.fit(X,Y)
+clf_perceptron.fit(X,Y)
+clf_KNN.fit(X,Y)
 
+prediction =clf_tree.predict([[190, 70, 43]])
 print(prediction)
+
+# Testing using the same data
+pred_tree=clf_tree.predict(X)
+acc_tree= accuracy_score(Y,pred_tree)*100
+print('Accuracy for DecisionTree: {}'.format(acc_tree))
+
+
+pred_per=clf_perceptron.predict(X)
+acc_per=accuracy_score(Y,pred_per)*100
+print('Accuracy for Perception: {}'.format(acc_per))
+
+pred_svm=clf_svm.predict(X)
+acc_svm=accuracy_score(Y,pred_svm)*100
+print('Accuracy for SVM: {}'.format(acc_svm))
+
+
+pred_KNN=clf_KNN.predict(X)
+acc_KNN=accuracy_score(Y,pred_KNN)*100
+print('Accuracy for KNN {}'.format(acc_KNN))
+
+
+## the best classifier SVM,per, KNN
+index=np.argmax([acc_svm,acc_per,acc_KNN])
+classifiers = {0: 'SVM',1: 'Perceptron',2: 'KNN'}
+print('Best Gender classifier is {}'.format(classifiers[index]))
+
+
